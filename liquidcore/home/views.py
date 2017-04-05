8d81c57a9f0b290e15a9e4d9a6f511d5f3aac440
Alex Morega <alex@grep.ro>
2017-04-05 13:53:14 +0300

    link to hypothesis app

--- liquidcore/home/views.py
+++ liquidcore/home/views.py
@@ -1,8 +1,11 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.conf import settings
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    return render(request, 'homepage.html', {
+        'hypothesis_app_url': settings.HYPOTHESIS_APP_URL,
+    })
 
 def profile(request):
     user = request.user
--- liquidcore/site/settings/example_local.py
+++ liquidcore/site/settings/example_local.py
@@ -1,3 +1,4 @@
 SECRET_KEY = 'FIME: generate random string'
 DEBUG = True
 HYPOTHESIS_CREATE_USER_SCRIPT = '/path/to/create_user.sh'
+HYPOTHESIS_APP_URL = 'http://hypothesis.odroid.liquid'
