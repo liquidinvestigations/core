174ad53ddb1c04e5b8448d3a896ab5eb48e91908
Alex Morega <alex@grep.ro>
2017-04-24 16:07:44 +0300

    check username and password

---
+++ liquidcore/home/management/commands/checkuser.py
@@ -0,0 +1,18 @@
+import sys
+from django.core.management.base import BaseCommand
+from django.contrib.auth import authenticate
+
+class Command(BaseCommand):
+
+    help = "Check if username and password are correct"
+
+    def add_arguments(self, parser):
+        parser.add_argument('username')
+        parser.add_argument('password')
+
+    def handle(self, username, password, **options):
+        user = authenticate(username=username, password=password)
+        if user is None:
+            sys.exit(1)
+        else:
+            sys.exit(0)
