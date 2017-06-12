2a42e54fd124dde78041d78a3c03ff4c53a7bec8
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-06-12 16:09:26 +0300

    update views to show discovered hostnames

--- liquidcore/home/views.py
+++ liquidcore/home/views.py
@@ -2,10 +2,13 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.conf import settings
 
+from . import discovery
+
 def homepage(request):
     return render(request, 'homepage.html', {
         'hypothesis_app_url': settings.HYPOTHESIS_APP_URL,
         'hoover_app_url': settings.HOOVER_APP_URL,
+        'node_hostnames': [node['hostname'] for node in discovery.nodes.values()],
     })
 
 def profile(request):
@@ -16,3 +19,9 @@ def profile(request):
         'login': user.get_username(),
         'email': user.email,
     })
+
+def nodes(request):
+    user = request.user
+    if not user.is_authenticated():
+        return HttpResponse('Unauthorized', status=401)
+    return JsonResponse(discovery.nodes)
