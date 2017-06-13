fecdb9ef1bb90c2f62573df424d3e234542e7374
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-06-13 17:48:42 +0300

    fix bytes dict

--- liquidcore/home/discovery.py
+++ liquidcore/home/discovery.py
@@ -1,20 +1,26 @@
 import socket
 from datetime import datetime
-from zeroconf import ServiceBrowser, Zeroconf, ServiceInfo
+from zeroconf import ServiceBrowser, Zeroconf, ServiceInfo, NonUniqueNameException
 from django.conf import settings
 
 nodes = {}
 
-def dict_string(data):
+def dict_decode(data):
+    def decode(data):
+        if isinstance(data, bytes):
+            return data.decode("latin-1")
+        elif isinstance(data, str):
+            return data
+        return str(data)
+
     return {
-            str(key): str(data[key])
+            decode(key): decode(data[key])
             for key in data
     }
 
-def get_data_from_info(info):
-    properties = dict_string(info.properties)
+def get_data_from_info(info, properties):
     hostname = properties['liquid_hostname']
-    data = {
+    return {
         "type": info.type,
         "server": info.server,
         "hostname": hostname,
@@ -26,32 +32,25 @@ def get_data_from_info(info):
         "properties": properties,
         "discovered_at": datetime.now().isoformat()
     }
-    return dict_string(data)
 
 class WorkstationListener(object):
     def add_service(self, zeroconf, type_, name):
         info = zeroconf.get_service_info(type_, name, 10000)
-        if info and 'liquid_hostname' in dict_string(info.properties):
-            nodes[name] = get_data_from_info(info)
+        if not info:
+            return
+        properties = dict_decode(info.properties)
+        if 'liquid_hostname' in properties:
+            nodes[name] = get_data_from_info(info, properties)
 
     def remove_service(self, zeroconf, type_, name):
-        info = zeroconf.get_service_info(type_, name)
         if name in nodes:
             del nodes[name]
 
 zeroconf = None
 
-def start():
-    global zeroconf
+SERVICE_TYPE = "_http._tcp.local."
 
-    zeroconf = Zeroconf()
-    listener = WorkstationListener()
-
-    SERVICE_TYPE = "_http._tcp.local."
-
-    # start a browser that listens for our service type
-    browser = ServiceBrowser(zeroconf, SERVICE_TYPE, listener)
-
+def register_service():
     # broadcast our service type
     service_hostname = settings.LIQUID_DOMAIN
     service_name = "LI_" + service_hostname.replace(".", "-") + '.' + SERVICE_TYPE
@@ -68,3 +67,23 @@ def start():
             server=service_local_hostname
     )
     zeroconf.register_service(liquid_service)
+
+def start():
+    global zeroconf
+
+    if nodes:
+        # this has been run before
+        return
+
+    zeroconf = Zeroconf()
+    listener = WorkstationListener()
+
+    # start a browser that listens for our service type
+    browser = ServiceBrowser(zeroconf, SERVICE_TYPE, listener)
+
+    # try to register the service
+    try:
+        register_service()
+    except NonUniqueNameException:
+        # The service has already been published
+        pass
