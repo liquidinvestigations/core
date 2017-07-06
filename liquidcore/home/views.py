bf5df3d310f0aa67daf427c4b12b2d92a4243bc1
Alex Morega <alex@grep.ro>
2017-07-06 13:54:51 +0300

    link to dokuwiki

--- liquidcore/home/views.py
+++ liquidcore/home/views.py
@@ -6,6 +6,7 @@ def homepage(request):
     return render(request, 'homepage.html', {
         'hypothesis_app_url': settings.HYPOTHESIS_APP_URL,
         'hoover_app_url': settings.HOOVER_APP_URL,
+        'dokuwiki_app_url': settings.DOKUWIKI_APP_URL,
     })
 
 def profile(request):
