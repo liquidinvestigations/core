93c93423189d4f4ee10d81a524d27d10fbfad5c2
Alex Morega <alex@grep.ro>
2017-10-11 22:00:09 +0300

    dump configuration as json

--- liquidcore/config/system.py
+++ liquidcore/config/system.py
@@ -1,2 +1,30 @@
+import json
+from . import models
+
+
+def get_configuration():
+    def map_table(model):
+        return {item.name: item.data for item in model.objects.all()}
+
+    db = {
+        'services': map_table(models.Service),
+        'nodes': map_table(models.Node),
+    }
+
+    settings = map_table(models.Setting)
+
+    return {
+        'domain': settings['domain']['domain'],
+        'lan': settings['lan'],
+        'wan': settings['wan'],
+        'ssh': settings['ssh'],
+        'services': {
+            s.name: {'enabled': s.is_enabled}
+            for s in models.Service.objects.all()
+        },
+    }
+
+
 def update_system():
-    print('TODO update system')
+    configuration = get_configuration()
+    print('TODO update system', json.dumps(configuration, indent=2))
