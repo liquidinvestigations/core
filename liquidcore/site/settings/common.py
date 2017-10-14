a891880ab0f56f0d62b66bcf096f24c7289c07a0
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-10-14 12:26:40 +0300

    put django csrf back in

--- liquidcore/site/settings/common.py
+++ liquidcore/site/settings/common.py
@@ -22,7 +22,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'oauth2_provider.middleware.OAuth2TokenMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
