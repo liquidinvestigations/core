de0fbf8d4a4d0061efa87dc7ac27831aba0ad1af
Alex Morega <alex@grep.ro>
2017-10-15 19:11:50 +0300

    make sure jobs wait for each other to finish

--- testsuite/test_reconfigure.py
+++ testsuite/test_reconfigure.py
@@ -10,6 +10,7 @@ def mock_target_configuration(monkeypatch):
     value = {}
 
     def set_value(new_value):
+        nonlocal value
         value = new_value
 
     monkeypatch.setattr(system, 'get_configuration', lambda: value)
@@ -39,16 +40,18 @@ def setup(tmpdir, monkeypatch):
     setup_configure = libexec / 'liquid-core-configure'
 
     class setup:
-        def mock(succeeds=True):
+        def mock(content=None, succeeds=True):
             by_outcome = {
                 True: MOCK_LIQUID_CORE_CONFIG,
                 False: MOCK_FAIL_LIQUID_CORE_CONFIG,
             }
             with setup_configure.open('w', encoding='utf8') as f:
-                f.write(by_outcome[succeeds])
+                f.write(content or by_outcome[succeeds])
             setup_configure.chmod(0o755)
 
     setup.mock()
+
+    setup.core_var_dir = core_var_dir
 
     with override_settings():
         settings.LIQUID_CORE_VAR = str(core_var_dir)
@@ -94,3 +97,44 @@ def test_failed_job_marks_system_as_broken(setup):
 
     reconfigure()
     reconfigure()
+
+
+TEST_CONCURRENCY_WHOAMI = """\
+#!/usr/bin/env python3
+import time
+import sys
+import json
+
+options = json.load(sys.stdin)
+
+def stamp_time(n):
+    filename = '{}{}.txt'.format(options['prefix'], n)
+    with open(filename, 'w', encoding='utf8') as f:
+        print(time.time(), file=f)
+
+stamp_time(0)
+time.sleep(3)
+stamp_time(1)
+"""
+
+
+def test_concurrency(setup, mock_target_configuration):
+    setup.mock(TEST_CONCURRENCY_WHOAMI)
+
+    mock_target_configuration({'prefix': '{}/a'.format(setup.core_var_dir)})
+    job1 = system.reconfigure_system()
+    mock_target_configuration({'prefix': '{}/b'.format(setup.core_var_dir)})
+    job2 = system.reconfigure_system()
+
+    job1.wait()
+    job2.wait()
+
+    with job1.open_logfile() as f: print(f.read())
+    with job2.open_logfile() as f: print(f.read())
+
+    def t(name):
+        path = setup.core_var_dir / (name + '.txt')
+        with path.open(encoding='utf8') as f:
+            return float(f.read())
+
+    assert t('a0') < t('a1') < t('b0') < t('b1')
