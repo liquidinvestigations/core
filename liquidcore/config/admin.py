a3200fe6e3d4ba947611cf999f77eef375cb8d71
Alex Morega <alex@grep.ro>
2017-10-30 20:41:23 +0200

    protect the `/reconfigure` pages

--- liquidcore/config/admin.py
+++ liquidcore/config/admin.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
 from . import agent
 
 
+@staff_member_required
 def reconfigure(request):
     jobs = sorted(agent.status().items())
     return render(request, 'liquidcore/config.html', {
@@ -10,6 +12,7 @@ def reconfigure(request):
     })
 
 
+@staff_member_required
 def reconfigure_job_log(request, job_id):
     job = agent.status()[job_id]['job']
     with job.open_logfile() as f:
