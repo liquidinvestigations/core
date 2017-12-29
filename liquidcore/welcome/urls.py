2030a5481a64d16a9c2b447e507b61ff1a65da22
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-12-29 14:24:59 +0200

    also mark welcome_started besides welcome_done

--- liquidcore/welcome/urls.py
+++ liquidcore/welcome/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^done/$', views.welcome_done),
     url(r'^$', views.welcome),
 ]
--- liquidcore/welcome/views.py
+++ liquidcore/welcome/views.py
@@ -1,17 +1,38 @@
 import json
 from pathlib import Path
 from django.shortcuts import render
-from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from ..config.models import Setting
 from ..config.system import reconfigure_system
+from django.http import JsonResponse
 
 WELCOME_DONE = Path('/var/lib/liquid/core/welcome_done')
+WELCOME_STARTED = Path('/var/lib/liquid/core/welcome_started')
 USERS_FILE = Path('/var/lib/liquid/core/users.json')
 
 
 def should_welcome():
     return not WELCOME_DONE.is_file()
+
+
+def has_welcome_started():
+    return WELCOME_STARTED.is_file()
+
+
+def get_welcome_domain():
+    with WELCOME_STARTED.open('r') as f:
+        return f.read()
+
+
+def welcome_done(request):
+    data = {
+        "done": not should_welcome(),
+        "started": has_welcome_started(),
+    }
+    if data['started']:
+        data['domain'] = get_welcome_domain()
+
+    return JsonResponse(data)
 
 
 def welcome(request):
@@ -33,8 +54,15 @@ def welcome(request):
 
         reconfigure_system()
 
-        return render(request, 'welcome-applying.html', {
-            'url': 'http://' + request.POST['domain'],
-        })
+        with WELCOME_STARTED.open('w') as f:
+            f.write(request.POST['domain'])
 
-    return render(request, 'welcome.html')
+        return HttpResponseRedirect('/welcome/')
+
+    elif request.method == 'GET':
+        if has_welcome_started():
+            return render(request, 'welcome-applying.html', {
+                'url': 'http://' + get_welcome_domain(),
+            })
+        else:
+            return render(request, 'welcome.html')
