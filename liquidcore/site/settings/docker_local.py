fd46d0ce3a4cecb14d12bdc3e7c17f2a303f635c
Alex Morega <alex@grep.ro>
2019-04-21 18:21:31 +0300

    Get configuration from environment vars in docker

--- liquidcore/site/settings/docker_local.py
+++ liquidcore/site/settings/docker_local.py
@@ -1,7 +1,9 @@
-import re
+DEBUG = os.environ.get('DEBUG', '').lower() in ['yes', 'true', '1', 'on']
 
-DEBUG = True
-SECRET_KEY = 'foo'
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+host = os.environ.get('HTTP_HOST')
+ALLOWED_HOSTS = [host] if host else []
+
 AUTH_PASSWORD_VALIDATORS = []
 INVOKE_HOOK = 'echo'
