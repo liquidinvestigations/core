55514803eddd25eb505e2d5c25c2192f44c38dc5
Alex Morega <alex@grep.ro>
2019-05-12 00:22:44 +0300

    More whitespace

--- liquidcore/home/views.py
+++ liquidcore/home/views.py
@@ -5,6 +5,7 @@ from django.utils.dateparse import parse_datetime
 
 from . import discover
 
+
 def homepage(request):
     return render(request, 'homepage.html', {
         'hypothesis_app_url': settings.HYPOTHESIS_APP_URL,
@@ -14,10 +15,13 @@ def homepage(request):
         'davros_app_url': settings.DAVROS_APP_URL,
     })
 
+
 def profile(request):
     user = request.user
+
     if not user.is_authenticated():
         return HttpResponse('Unauthorized', status=401)
+
     return JsonResponse({
         'login': user.get_username(),
         'email': user.email,
