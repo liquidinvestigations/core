559bd8c2e4b4c55ba446d04763bd463149c7763b
Alex Morega <alex@grep.ro>
2019-05-25 14:47:23 +0300

    Restore the admin

--- liquidcore/site/urls.py
+++ liquidcore/site/urls.py
@@ -1,12 +1,11 @@
+from django.contrib import admin
 from django.urls import path, include
 from ..home import views
 
-
-def _urlpatterns():
-    yield path('accounts/', include('django.contrib.auth.urls'))
-    yield path('accounts/profile', views.profile)
-    yield path('o/', include('oauth2_provider.urls', namespace='oauth2_provider'))
-    yield path('', views.homepage)
-
-
-urlpatterns = list(_urlpatterns())
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile', views.profile),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('', views.homepage),
+]
