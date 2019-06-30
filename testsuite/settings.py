05f17e2958c8145acdda63bb9a1d01a75f2e9220
Radu Capalb <radu_capalb98@yahoo.com>
2019-07-01 01:28:06 +0300

    Test to see what causes test step to fail

--- testsuite/settings.py
+++ testsuite/settings.py
@@ -1,6 +1,7 @@
-from liquidcore.site.settings import *  # noqa: F401, F403
 import os
 os.environ['LIQUID_DOMAIN'] = 'liquid.example.org'
 os.environ['LIQUID_TITLE'] = 'Liquid Example Org'
 os.environ['SECRET_KEY'] = 'sheap'
 os.environ['LIQUID_2FA'] = 'true'
+
+from liquidcore.site.settings import *  # noqa: F401, F403
