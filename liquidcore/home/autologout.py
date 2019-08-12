86b68171698f4e3e6862718133660013c7286f45
Alex Morega <alex@grep.ro>
2019-08-12 12:06:45 +0300

    Use `user.login_time` to calculate logout time

--- liquidcore/home/autologout.py
+++ liquidcore/home/autologout.py
@@ -1,32 +1,43 @@
-from time import time
+from datetime import timedelta
 from django.conf import settings
 from django.contrib.auth import logout
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
 from django.http import HttpResponseRedirect
+from django.utils import timezone
 
-login_time_session_key = 'hoover.contrib.twofactor.login_time'
+
+def get_interval(spec):
+    if not spec:
+        return None
+
+    if spec.endswith('d'):
+        return timedelta(days=int(spec[:-1]))
+
+    if spec.endswith('h'):
+        return timedelta(hours=int(spec[:-1]))
+
+    if spec.endswith('m'):
+        return timedelta(minutes=int(spec[:-1]))
+
+    if spec.endswith('s'):
+        return timedelta(seconds=int(spec[:-1]))
+
+    raise RuntimeError(f"Can't parse interval {spec!r}")
 
 
-@receiver(user_logged_in)
-def on_login_success(sender, request, **kwargs):
-    request.session[login_time_session_key] = time()
+auto_logout = get_interval(settings.AUTH_AUTO_LOGOUT)
 
 
 def AutoLogoutMiddleware(get_response):
-    auto_logout = settings.AUTH_AUTO_LOGOUT
     if not auto_logout:
         return get_response
-
-    assert auto_logout.endswith('h')
-    interval = int(auto_logout[:-1]) * 3600
 
     def middleware(request):
         user = request.user
         if user.is_authenticated:
-            login_time = request.session.get(login_time_session_key) or 0
-            logout_time = login_time + interval
-            if time() > logout_time:
+            login_time = user.last_login
+            logout_time = login_time + auto_logout
+            now = timezone.now()
+            if now > logout_time:
                 logout(request)
                 login = "{}?next={}".format(settings.LOGIN_URL, request.path)
                 return HttpResponseRedirect(login)
