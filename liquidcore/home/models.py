965571bded646c2eda430bd93e8c93e2bc0ea2bb
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-09-18 16:56:57 +0300

    switch to custom user model and validate username

---
+++ liquidcore/home/models.py
@@ -0,0 +1,12 @@
+import string
+from django.contrib.auth.models import AbstractUser
+
+USERNAME_CHARS = string.ascii_letters + string.digits + '.'
+
+class User(AbstractUser):
+    class Meta:
+        db_table='auth_user'
+
+    def save(self, *args, **kwargs):
+        assert all(x in USERNAME_CHARS for x in self.username), 'Bad Username'
+        super(User, self).save(*args, **kwargs)
--- liquidcore/site/settings.py
+++ liquidcore/site/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
 AUTH_STAFF_ONLY = bool_env(os.environ.get('AUTH_STAFF_ONLY'))
 
 AUTH_AUTO_LOGOUT = os.environ.get('AUTH_AUTO_LOGOUT')
+AUTH_USER_MODEL = 'home.User'
 
 if LIQUID_2FA:
     INSTALLED_APPS += [
