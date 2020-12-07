a0452aae0428b36b55fdeb23be9478901559e552
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-12-07 16:51:49 +0200

    kill sessions on expiration instead of redirect

--- liquidcore/home/autologout.py
+++ liquidcore/home/autologout.py
@@ -1,25 +1,22 @@
-from datetime import timedelta
 from django.conf import settings
 from django.contrib.auth import logout
-from django.http import HttpResponseRedirect
 from django.utils import timezone
+
+from .auth import kill_sessions
 
 
 def AutoLogoutMiddleware(get_response):
-    if not settings.AUTH_AUTO_LOGOUT:
-        return get_response
-
     def middleware(request):
         user = request.user
         if user.is_authenticated:
             login_time = user.last_login
             logout_time = login_time + \
-                settings.AUTH_AUTO_LOGOUT + timedelta(minutes=59)
+                settings.AUTH_AUTO_LOGOUT
             now = timezone.now()
             if now > logout_time:
                 logout(request)
-                login = "{}?next={}".format(settings.LOGIN_URL, request.path)
-                return HttpResponseRedirect(login)
+                kill_sessions(user)
+                request.session.clear()
 
         return get_response(request)
 
