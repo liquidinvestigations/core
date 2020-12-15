a8a4ef9938040eacf29f1c4a3b013cc5dc2a9478
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-12-15 14:37:00 +0200

    add yasg routes

--- liquidcore/site/urls.py
+++ liquidcore/site/urls.py
@@ -1,7 +1,12 @@
 from django.conf import settings
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from .admin import liquid_admin
 from ..home import views
+
 
 urlpatterns = [
     path('admin/', liquid_admin.urls),
@@ -22,3 +27,27 @@ if settings.LIQUID_2FA:
         path('accounts/login/', login_view),
         path('invitation/<code>', twofactor_views.invitation),
     ] + urlpatterns
+
+# DRF-YASG
+# ========
+if settings.DEBUG:
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Core API",
+            default_version='v0',
+            # description="Liquid API for Tags",
+            # contact=openapi.Contact(email="contact@liquiddemo.org"),
+            # license=openapi.License(name="MIT License"),
+        ),
+        public=True,
+        permission_classes=[permissions.AllowAny],
+    )
+
+    schema_urlpatterns = [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+                schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
+
+    urlpatterns += schema_urlpatterns
