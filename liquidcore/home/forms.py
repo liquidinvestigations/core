faa11cbdb90fa46462f1c096b124e2469e6cc50b
Kjell Knudsen <kjell.knudsen@yahoo.de>
2021-05-12 17:00:25 +0200

    add token field for password change

---
+++ liquidcore/home/forms.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.forms import PasswordChangeForm
+from django import forms
+from django.utils.translation import gettext, gettext_lazy as _
+from django.core.exceptions import ValidationError
+import django_otp
+
+
+class OtpPasswordChangeForm(PasswordChangeForm):
+
+    error_messages = {
+        **PasswordChangeForm.error_messages,
+        'token_incorrect': _("Your token was entered incorrectly. Please enter it again."),
+    }
+
+    otp_token = forms.CharField(
+        label=_("OTP token"),
+        widget=forms.TextInput())
+
+    def clean_otp_token(self):
+        otp_token = self.cleaned_data["otp_token"]
+        if not django_otp.match_token(self.user, otp_token):
+            raise ValidationError(
+                self.error_messages['token_incorrect'],
+                code='token_incorrect',
+                    )
+        return otp_token
--- liquidcore/site/urls.py
+++ liquidcore/site/urls.py
@@ -2,10 +2,15 @@ from django.conf import settings
 from django.urls import path, include
 from .admin import liquid_admin
 from ..home import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', liquid_admin.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/profile', views.profile),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('', views.homepage),
@@ -15,11 +20,13 @@ if settings.LIQUID_2FA:
     from django.contrib.auth.views import LoginView
     from django_otp.forms import OTPAuthenticationForm
     from ..twofactor import views as twofactor_views
+    from ..home.forms import OtpPasswordChangeForm
 
     login_view = LoginView.as_view(authentication_form=OTPAuthenticationForm)
 
     urlpatterns = [
         path('accounts/login/', login_view),
+        path('accounts/password_change/', auth_views.PasswordChangeView.as_view(form_class=OtpPasswordChangeForm), name='password_change'),
         path('invitation/<code>', twofactor_views.invitation),
         path('accounts/change_totp/', twofactor_views.change_totp),
         path('accounts/change_totp/confirm',
