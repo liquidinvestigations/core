ab0730e536374c0baeb93c01fe9d62cd17dc1cc8
Kjell Knudsen <kjell.knudsen@yahoo.de>
2021-06-02 17:53:14 +0200

    add command to clean unconfirmed devices

---
+++ liquidcore/twofactor/management/commands/clean_devices.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from ... import invitations
+from django_otp.plugins.otp_totp.models import TOTPDevice
+
+
+def remove_unconfirmed_devices():
+    TOTPDevice.objects.all().filter(confirmed=False).delete()
+
+
+class Command(BaseCommand):
+
+    help = "Remove unconfirmed devices."
+
+    def handle(self, *args, **options):
+        remove_unconfirmed_devices()
