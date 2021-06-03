62451ec6921c7cc30c29140bfe7dc6d9be86dd8f
Kjell Knudsen <kjell.knudsen@yahoo.de>
2021-06-03 12:20:16 +0200

    create payload function for tests

--- testsuite/test_login.py
+++ testsuite/test_login.py
@@ -2,7 +2,7 @@ import pytest
 from datetime import timedelta
 from django.urls import reverse
 from django.utils.timezone import now
-from conftest import _totp, is_logged_in, _reset_last_use
+from conftest import _totp, is_logged_in, _reset_last_use, payload
 from time import sleep
 
 pytestmark = pytest.mark.django_db
@@ -25,61 +25,48 @@ def test_login_page(client):
 
 def test_login_no_totp(client, settings, create_user):
     settings.LIQUID_2FA = False
-    client.post(get_url(), {
-        'username': create_user.user.get_username(),
-        'password': create_user.password,
-    })
+    client.post(get_url(), payload(create_user.user.get_username(),
+                                   create_user.password))
     assert is_logged_in(client)
     client.logout()
     assert not is_logged_in(client)
 
-    client.post(get_url(), {
-        'username': 'badusername',
-        'password': create_user.password,
-    })
+    client.post(get_url(), payload('badusername',
+                                   create_user.password))
     assert not is_logged_in(client)
 
-    client.post(get_url(), {
-        'username': create_user.user.get_username(),
-        'password': 'badpassword',
-    })
+    client.post(get_url(), payload(create_user.user.get_username(),
+                                   'badpassword'))
     assert not is_logged_in(client)
 
 
 def test_login_totp(client, create_user, create_device):
     device = create_device(user=create_user.user)
-    client.post(get_url(), {
-        'username': 'badusername',
-        'password': create_user.password,
-        'otp_token': _totp(device, now())
-    })
+    client.post(get_url(), payload('badusername',
+                                   create_user.password,
+                                   _totp(device, now())))
     assert not is_logged_in(client)
     sleep(30)
 
     _reset_last_use(device)
-    client.post(get_url(), {
-        'username': create_user.user.get_username(),
-        'password': 'badpassword',
-        'otp_token': _totp(device, now())
-    })
+    client.post(get_url(), payload(create_user.user.get_username(),
+                                   'badpassword',
+                                   _totp(device, now())))
     assert not is_logged_in(client)
 
     sleep(30)
     _reset_last_use(device)
-    client.post(get_url(), {
-        'username': create_user.user.get_username(),
-        'password': create_user.password,
-        'otp_token': _totp(device, now() + timedelta(minutes=5))
-    })
+    client.post(get_url(), payload(create_user.user.get_username(),
+                                   create_user.password,
+                                   _totp(device, now() +
+                                         timedelta(minutes=5))))
     assert not is_logged_in(client)
 
     sleep(30)
     _reset_last_use(device)
-    client.post(get_url(), {
-        'username': create_user.user.get_username(),
-        'password': create_user.password,
-        'otp_token': _totp(device, now())
-    })
+    client.post(get_url(), payload(create_user.user.get_username(),
+                                   create_user.password,
+                                   _totp(device, now())))
     assert is_logged_in(client)
     client.logout()
     assert not is_logged_in(client)
--- testsuite/test_password_change.py
+++ testsuite/test_password_change.py
@@ -7,29 +7,34 @@ def get_url():
     return reverse('password_change')
 
 
+def payload_pw_change(oldpw, newpw1, newpw2, otp_token=None):
+    payload = {
+        'old_password': oldpw,
+        'new_password1': newpw1,
+        'new_password2': newpw2,
+    }
+    if otp_token:
+        payload['otp_token'] = otp_token
+    return payload
+
+
 def test_password_change_no_totp(settings, client, create_user):
     settings.LIQUID_2FA = False
     client.login(username=create_user.user.get_username(),
                  password=create_user.password)
-    resp = client.post(get_url(), {
-        'old_password': 'badpassword',
-        'new_password1': 'new_password',
-        'new_password2': 'new_password',
-    })
+    resp = client.post(get_url(), payload_pw_change('badpassword',
+                                                    'new_password',
+                                                    'new_password'))
     assert not resp.context.get('form').is_valid()
 
-    resp = client.post(get_url(), {
-        'old_password': create_user.password,
-        'new_password1': 'new_password',
-        'new_password2': 'bad_new_password',
-    })
+    resp = client.post(get_url(), payload_pw_change(create_user.password,
+                                                    'new_password',
+                                                    'bad_new_password'))
     assert not resp.context.get('form').is_valid()
 
-    client.post(get_url(), {
-        'old_password': create_user.password,
-        'new_password1': 'new_password',
-        'new_password2': 'new_password',
-    })
+    resp = client.post(get_url(), payload_pw_change(create_user.password,
+                                                    'new_password',
+                                                    'new_password'))
     client.logout()
     client.login(username=create_user.user.get_username(),
                  password=create_user.password)
@@ -44,21 +49,18 @@ def test_password_change_totp(settings, client, create_user, create_device):
                  password=create_user.password)
     assert is_logged_in(client)
 
-    resp = client.post(get_url(), {
-        'old_password': create_user.password,
-        'new_password1': 'new_password',
-        'new_password2': 'new_password',
-        'otp_token': 999999,
-    })
+    resp = client.post(get_url(), payload_pw_change(create_user.password,
+                                                    'new_password',
+                                                    'new_password',
+                                                    999999))
     assert not resp.context.get('form').is_valid()
 
     device = create_device(user=create_user.user)
-    resp = client.post(get_url(), {
-        'old_password': create_user.password,
-        'new_password1': 'new_password',
-        'new_password2': 'new_password',
-        'otp_token': _totp(device, now())
-    })
+
+    resp = client.post(get_url(), payload_pw_change(create_user.password,
+                                                    'new_password',
+                                                    'new_password',
+                                                    _totp(device, now())))
     client.logout()
     client.login(username=create_user.user.get_username(),
                  password=create_user.password)
