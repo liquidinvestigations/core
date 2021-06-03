21f8bc954cba9fec6501a0e471c6d9c9a0c6e826
Kjell Knudsen <kjell.knudsen@yahoo.de>
2021-06-03 15:58:04 +0200

    flake migration

--- liquidcore/twofactor/migrations/0002_totpdevicetimed.py
+++ liquidcore/twofactor/migrations/0002_totpdevicetimed.py
@@ -15,7 +15,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='TOTPDeviceTimed',
             fields=[
-                ('totpdevice_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='otp_totp.totpdevice')),
+                ('totpdevice_ptr',
+                 models.OneToOneField(auto_created=True,
+                                      on_delete=django.db
+                                      .models.deletion.CASCADE,
+                                      parent_link=True,
+                                      primary_key=True,
+                                      serialize=False,
+                                      to='otp_totp.totpdevice')),
                 ('created', models.DateTimeField(auto_now_add=True)),
             ],
             options={
