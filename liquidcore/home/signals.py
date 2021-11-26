f299550b35451c7ff18473161aee3950fc04e7e6
k-jell <42188452+k-jell@users.noreply.github.com>
2021-11-26 09:22:28 +0100

    add trigger for nomad user creation job (#187)
    * trigger liquid user creation when new core user is saved
    
    * get nomad ip from env
    
    * only create liquid users when nomad ip exists
    
    * flake
    
    * use nomad url env
    
    * fix variable name

--- liquidcore/home/signals.py
+++ liquidcore/home/signals.py
@@ -1,11 +1,16 @@
+import logging
 import string
+
+import requests
+import os
 from django.contrib.auth import get_user_model
 from django.contrib.auth.signals import user_logged_out
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+
 from .auth import kill_sessions
 
-
+log = logging.getLogger(__name__)
 USERNAME_CHARS = string.ascii_letters + string.digits + '.'
 User = get_user_model()
 
@@ -19,3 +24,28 @@ def kill_user_sessions(user, **kwargs):
 def check_user_username(sender, instance, **kwargs):
     assert all(x in USERNAME_CHARS for x in instance.username), \
         f'Bad username: "{instance.username}"'
+
+
+@receiver(post_save, sender=User)
+def create_user_everywhere(sender, instance, created=None, **kwargs):
+    if created:
+        payload = {
+            "Meta": {
+                "USERNAME": instance.username
+            }
+        }
+        nomad_url = os.getenv('NOMAD_URL')
+        if nomad_url:
+            res = requests.post(
+                f'{nomad_url}/v1/job/liquid-createuser/dispatch',
+                json=payload
+            )
+            if res.status_code != 200:
+                log.warning(f'Error creating user "{instance.username}".')
+                log.warning(
+                    f'Nomad returned: {str(res.status_code)}, {res.text}'
+                )
+            else:
+                log.warning(f'Created liquid user "{instance.username}".')
+        else:
+            log.warning('No nomad address found to create liquid users!')
