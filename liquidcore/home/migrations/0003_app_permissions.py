9ba9d19e6f649b79757ebbe6455fd8a508817a75
Kjell Knudsen <kjell.knudsen@yahoo.de>
2022-04-13 14:55:59 +0200

    fix migrations to create permissions during the process

--- liquidcore/home/migrations/0003_app_permissions.py
+++ liquidcore/home/migrations/0003_app_permissions.py
@@ -1,24 +1,29 @@
 from django.db import migrations
 from django.contrib.auth.models import User, Permission
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from ..models import AppPermissions
 
 
 class Migration(migrations.Migration):
 
     def add_user_permissions(apps, schema_editor):
-        for x in Permission.objects.all():
-            print(x)
+        content_type = ContentType.objects.get_for_model(AppPermissions)
         for user in User.objects.all():
             for liquid_app in settings.LIQUID_APPS:
                 id = liquid_app['id']
                 if liquid_app['adminOnly']:
                     continue
-                permission = Permission.objects.get(codename=f'use_{id}')
+                permission, _ = Permission.objects.get_or_create(
+                    codename=f'use_{id}',
+                    name=f'Can use {id}',
+                    content_type=content_type
+                )
                 user.user_permissions.add(permission)
                 user.save()
 
     dependencies = [
-        ('home', '0003_auto_20220406_1047'),
+        ('home', '0002_apppermissions'),
     ]
 
     operations = [
