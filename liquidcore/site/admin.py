e24323517f7ceb0b74a2d772c8304ff40bc2dc86
Kjell Knudsen <kjell.knudsen@yahoo.de>
2022-04-19 17:05:44 +0200

    filter permissions in admin panel

--- liquidcore/site/admin.py
+++ liquidcore/site/admin.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.admin import User, UserAdmin
 from django.contrib.admin import site
+from django.forms import CheckboxSelectMultiple
 
 if settings.LIQUID_2FA:
     from django_otp.admin import OTPAdminSite as AdminSite
@@ -12,8 +13,38 @@ class HooverAdminSite(AdminSite):
     site_header = "Liquid Core administration"
 
 
-class HooverUserAdmin(UserAdmin):
+class PermissionFilterMixin(object):
+    '''Filter the permissions field in the admin panel to show only app permisssions.
+
+    Changes the manytomany formfield for the django permissions, by filtering
+    all permissions but the ones to allow app usage.
+    '''
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        if db_field.name in ('permissions', 'user_permissions'):
+            qs = kwargs.get('queryset', db_field.remote_field.model.objects)
+            qs = _filter_permissions(qs)
+            kwargs['queryset'] = qs
+
+        return super(PermissionFilterMixin, self).formfield_for_manytomany(db_field, request, **kwargs)
+
+
+def _filter_permissions(qs):
+    '''Filter the permission queryset to show only enabled apps.
+
+    Gets a queryset as input and filters it based on the LIQUID_APPS setting.
+    '''
+    return qs.filter(codename__in=(
+        [f'use_{perm}' for perm in [app['id'] for app in settings.LIQUID_APPS if app['enabled']]]
+    ))
+
+
+class HooverUserAdmin(PermissionFilterMixin, UserAdmin):
     actions = []
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(UserAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['user_permissions'].widget = CheckboxSelectMultiple()
+        return form
 
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
@@ -47,3 +78,7 @@ for model, model_admin in site._registry.items():
         continue
 
     liquid_admin.register(model, model_admin_cls)
+
+
+site.unregister(User)
+site.register(User, HooverUserAdmin)
