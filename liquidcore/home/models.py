39a3c765bc535818287d2192199d63e0da856691
k-jell <42188452+k-jell@users.noreply.github.com>
2022-04-21 16:53:22 +0200

    Permission admin (#191)
    * filter permissions in admin panel
    
    * change group admin and add permissions in list view
    
    * add group permissions to user admin panel
    
    * fix line break
    
    * only show the name as Permission string
    
    * disable oauth2 section in admin panel
    
    * remove print

--- liquidcore/home/models.py
+++ liquidcore/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import Permission
 
 
 class AppPermissions(models.Model):
@@ -12,3 +13,8 @@ class AppPermissions(models.Model):
             ('use_rocketchat', 'Can use rocketchat'),
             ('use_hypothesis', 'Can use hypothesis'),
         ]
+
+
+# we need to override the __str__ method to only display the name
+# instead of contenttype | name in the admin panel
+Permission.__str__ = lambda self: f'{self.name}'
--- liquidcore/site/admin.py
+++ liquidcore/site/admin.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-from django.contrib.auth.admin import User, UserAdmin
+from django.contrib.auth.admin import User, Group, UserAdmin, GroupAdmin
 from django.contrib.admin import site
+from django.forms import CheckboxSelectMultiple
 
 if settings.LIQUID_2FA:
     from django_otp.admin import OTPAdminSite as AdminSite
@@ -12,8 +13,62 @@ class HooverAdminSite(AdminSite):
     site_header = "Liquid Core administration"
 
 
-class HooverUserAdmin(UserAdmin):
+class PermissionFilterMixin(object):
+    '''Filter permissions field in the admin panel to show only app permisssions.
+
+    Changes the manytomany formfield for the django permissions, by filtering
+    all permissions but the ones to allow app usage.
+    '''
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        if db_field.name in ('permissions', 'user_permissions'):
+            qs = kwargs.get('queryset', db_field.remote_field.model.objects)
+            qs = _filter_permissions(qs)
+            kwargs['queryset'] = qs
+
+        return super(PermissionFilterMixin, self).formfield_for_manytomany(
+            db_field, request, **kwargs)
+
+
+def _filter_permissions(qs):
+    '''Filter the permission queryset to show only enabled apps.
+
+    Gets a queryset as input and filters it based on the LIQUID_APPS setting.
+    '''
+    return qs.filter(codename__in=(
+        [f'use_{perm}' for perm in
+         [app['id']for app in settings.LIQUID_APPS if app['enabled']]]
+    ))
+
+
+def all_permissions():
+    '''Helper function that returns a set of all app permissions as strings.'''
+    return {f'home.use_{perm}' for perm in
+            [app['id'] for app in settings.LIQUID_APPS
+             if app['enabled'] and not app['adminOnly']]}
+
+
+class HooverUserAdmin(PermissionFilterMixin, UserAdmin):
     actions = []
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(HooverUserAdmin, self).get_form(request, obj, **kwargs)
+        if 'user_permissions' in form.base_fields:
+            form.base_fields['user_permissions'].widget = (
+                CheckboxSelectMultiple())
+        return form
+
+    def user_app_permissions(self, obj):
+        return [perm.codename for perm in obj.user_permissions.all()]
+
+    def group_permissions(self, obj):
+        perm_set = obj.get_group_permissions()
+        if all_permissions().issubset(perm_set):
+            return 'All app permissions.'
+        if perm_set:
+            return [perm.split('.')[1] for perm in perm_set
+                    if perm in all_permissions()]
+        else:
+            return '-'
 
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
@@ -23,16 +78,32 @@ class HooverUserAdmin(UserAdmin):
                        'is_staff',
                        'is_superuser',
                        'groups',
-                       'user_permissions'),
+                       'group_permissions',
+                       'user_permissions',
+                       ),
         }),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     list_display = ('username', 'email', 'first_name', 'last_name',
-                    'is_staff', 'last_login')
+                    'is_staff', 'last_login', 'user_app_permissions')
+
+    readonly_fields = ('group_permissions',)
 
     if settings.LIQUID_2FA:
         from ..twofactor.invitations import create_invitations
         actions.append(create_invitations)
+
+
+class HooverGroupAdmin(PermissionFilterMixin, GroupAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(HooverGroupAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['permissions'].widget = CheckboxSelectMultiple()
+        return form
+
+    def group_app_permissions(self, obj):
+        return [perm.codename for perm in obj.permissions.all()]
+
+    list_display = ('name', 'group_app_permissions',)
 
 
 liquid_admin = HooverAdminSite(name='liquidadmin')
@@ -43,7 +114,13 @@ for model, model_admin in site._registry.items():
     if model is User:
         model_admin_cls = HooverUserAdmin
 
+    if model is Group:
+        model_admin_cls = HooverGroupAdmin
+
     if model._meta.app_label == 'otp_totp':
+        continue
+
+    if model._meta.app_label == 'oauth2_provider':
         continue
 
     liquid_admin.register(model, model_admin_cls)
