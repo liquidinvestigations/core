503f1f0ceb3732af058cece551526c95012ab6a3
Kjell Knudsen <kjell.knudsen@yahoo.de>
2022-04-27 11:16:01 +0200

    only delete sessions if redis url exists

--- liquidcore/home/sessions.py
+++ liquidcore/home/sessions.py
@@ -13,7 +13,9 @@ def clear_authproxy_session(username):
     '''Delete app sessions for all apps stored in redis for that user. '''
 
     for app_id in APP_REDIS_IDS.values():
-        r = redis.from_url('redis://' + settings.AUTHPROXY_REDIS_URL + app_id)
-        for key in r.scan_iter():
-            if r.get(key).startswith(username.encode()):
-                r.delete(key)
+        if settings.AUTHPROXY_REDIS_URL:
+            r = redis.from_url('redis://'
+                               + settings.AUTHPROXY_REDIS_URL + app_id)
+            for key in r.scan_iter():
+                if r.get(key).startswith(username.encode()):
+                    r.delete(key)
