fb543d621a850cbe24588a636a88743c2a0e3fd4
Kjell Knudsen <kjell.knudsen@yahoo.de>
2022-05-11 14:01:53 +0200

    fix test regex

--- testsuite/test_app_access.py
+++ testsuite/test_app_access.py
@@ -64,5 +64,5 @@ def test_html(client, create_user, use_liquid_apps):
 
 
 def find_enabled_link(html, app):
-    line_match = re.search(f'^.*{app}.*$', html)
-    return 'inactiveLink' not in line_match.group(0)
+    link_match = re.search(f'{app}" .*>', html)
+    return 'inactiveLink' not in link_match.group(0)
