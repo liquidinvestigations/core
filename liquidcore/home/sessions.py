8c2996f9f33e3704ba401bb01bcb2546cf2e1bdb
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2023-03-02 23:00:52 +0200

    pull redis id from settings

--- liquidcore/home/sessions.py
+++ liquidcore/home/sessions.py
@@ -1,17 +1,13 @@
 import redis
 from django.conf import settings
 
-APP_REDIS_IDS = {
-    'hoover': 1,
-    'dokuwiki': 2,
-    'codimd': 3,
-    'nextcloud': 4,
-}
-
-
 def clear_authproxy_session(username):
     '''Delete app sessions for all apps stored in redis for that user. '''
-
+    APP_REDIS_IDS = {
+        v['id']: v.get('redis_id')
+        for v in settings.LIQUID_APPS.values()
+        if v.get('redis_id')
+    }
     for app_id in APP_REDIS_IDS.values():
         if settings.AUTHPROXY_REDIS_URL:
             r = redis.from_url(settings.AUTHPROXY_REDIS_URL + str(app_id))
