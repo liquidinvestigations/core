8dca30176ccaecc7c792838f8e532e21fbb9dd65
Kjell Knudsen <kjell.knudsen@yahoo.de>
2023-11-10 12:08:26 +0100

    add new nextcloud permission
    add migration for new permission
    
    add onlyoffice permission if nextcloud is allowed
    
    add proxy permission for onlyoffice
    
    upgrade to nextcloud28
    
    squash migrations

--- liquidcore/home/models.py
+++ liquidcore/home/models.py
@@ -14,6 +14,7 @@ class AppPermissions(models.Model):
             ('use_rocketchat', 'Can use rocketchat'),
             ('use_hypothesis', 'Can use hypothesis'),
             ('use_wikijs', 'Can use Wiki.js'),
+            ('use_nextcloud28', 'Can use Nextcloud 28')
         ]
 
 
