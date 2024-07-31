8c32aa05404dd4fe9bf60500b2c239729885b4b2
Kjell Knudsen <kjell.k@posteo.net>
2024-07-31 11:10:14 +0200

    flake

--- liquidcore/home/management/commands/createdemouser.py
+++ liquidcore/home/management/commands/createdemouser.py
@@ -1,6 +1,4 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-from django.contrib.auth.models import Permission
 from ...demo import create_demo_user
 
 
